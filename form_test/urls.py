6b35a8ce7a23a861af72ebe32ed2500e064b2328
kohei <kohei@debian.tsujike.com>
2023-07-20 08:28:32 +0900

    Added confirmation screen

--- form_test/urls.py
+++ form_test/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('input/', views.InputView.as_view(), name='input'),
+    path('confirm/', views.ConfirmView.as_view(), name='confirm'),
     path('thankyou/', views.ThankyouView.as_view(), name='thankyou'),
 ]
--- form_test/views.py
+++ form_test/views.py
@@ -8,7 +8,18 @@ from .forms import PostForm
 class InputView(FormView):
     template_name = "form_test/input.html"
     form_class = PostForm
-    success_url = "/form_test/thankyou/"
+    success_url = "/form_test/confirm/"
+
+class ConfirmView(TemplateView):
+    template_name = "form_test/confirm.html"
+    def post(self, request, *args, **kwargs):
+        self.kwargs["name"] = request.POST["name"] 
+        self.kwargs["text"] = request.POST["text"] 
+        return render(request, self.template_name, context=self.kwargs)
 
 class ThankyouView(TemplateView):
     template_name = "form_test/thankyou.html"
+    def post(self, request, *args, **kwargs):
+        self.kwargs["name"] = request.POST["name"] 
+        self.kwargs["text"] = request.POST["text"] 
+        return render(request, self.template_name, context=self.kwargs)
